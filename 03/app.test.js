aa4a1112fe4330c8616a66ddd25f087178201f02
Sandra <sandra.mstowskaa@gmail.com>
2025-10-21 15:53:55 +0200

    task03/ test: add test for invalid range where min > max

--- 03/app.test.js
+++ 03/app.test.js
@@ -8,3 +8,7 @@ test('throw error if min or max are not numbers', () => {
     expect(() => randomNumber('a', 5)).toThrow('Arguments must be numbers');
     expect(() => randomNumber(1, null)).toThrow('Arguments must be numbers');
 });
+
+test('throw error if min is bigger then max', () => {
+    expect(() => randomNumber(5, 3)).toThrow('Min should be less or equal to max');
+});
