383a5b9a6495221214156ecfbd587f97a219173c
Sandra <sandra.mstowskaa@gmail.com>
2025-10-21 16:02:01 +0200

    task03/ test: add test for correct random number generation within range

--- 03/app.test.js
+++ 03/app.test.js
@@ -12,3 +12,11 @@ test('throw error if min or max are not numbers', () => {
 test('throw error if min is bigger then max', () => {
     expect(() => randomNumber(5, 3)).toThrow('Min should be less or equal to max');
 });
+
+test('generate random number from min to max', () => {
+    const min = 1;
+    const max = 10;
+    const value = randomNumber(min, max);
+    expect(value).toBeGreaterThanOrEqual(min);
+    expect(value).toBeLessThanOrEqual(max);
+});
