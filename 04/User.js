39877adac979f83f1dafd6fc31d4d0b935cc8774
Sandra <sandra.mstowskaa@gmail.com>
2025-10-21 16:32:58 +0200

    task04/ feat: add validation for password in User

--- 04/User.js
+++ 04/User.js
@@ -1,8 +1,12 @@
 export default class User {
     constructor({ email, password }) {
-        if(!this.email.includes('@')) {
-            throw new Error();
+        if(!email.includes('@')) {
+            throw new Error('Invalid email address');
         }
+        if(typeof password !== 'string' || password.length < 6) {
+            throw new Error('Password must be at least 6 characters long');
+        }
+
         this.email = email;
         this.password = password;
     }
