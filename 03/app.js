c7d8e82e3d1dbdc17a24c262bf28a63c9c7470a3
Sandra <sandra.mstowskaa@gmail.com>
2025-10-21 15:51:04 +0200

    task03/  feat: add check for non-number arguments in randomNumber

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,7 @@
 export default function randomNumber(min, max) {
+    if(typeof min !== 'number' || typeof max !== 'number') {
+        throw new Error('Arguments must be numbers');
+    }
+    if (min === max) return min;
     return min;
 }
