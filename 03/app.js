2baa647ab2acd19c3730be45beac3382ab6aa10c
Sandra <sandra.mstowskaa@gmail.com>
2025-10-21 15:55:53 +0200

    task03/ feat: add validation for min <= max in randomNUmber

--- 03/app.js
+++ 03/app.js
@@ -2,6 +2,9 @@ export default function randomNumber(min, max) {
     if(typeof min !== 'number' || typeof max !== 'number') {
         throw new Error('Arguments must be numbers');
     }
+    if (min > max) {
+        throw new Error('Min should be less or equal to max');
+    }
     if (min === max) return min;
     return min;
 }
