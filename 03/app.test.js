4a6fdcfd6bc377b9e7c2b73cec28b1bc59e31b60
Sandra <sandra.mstowskaa@gmail.com>
2025-10-21 15:47:43 +0200

    task03/ test: add test for non-number arguments throw error

--- 03/app.test.js
+++ 03/app.test.js
@@ -3,3 +3,8 @@ import randomNumber from './app';
 test('draws 1 when min and max are 1', () => {
     expect(randomNumber(1, 1)).toBe(1);
 });
+
+test('throw error if min or max are not numbers', () => {
+    expect(() => randomNumber('a', 5)).toThrow('Arguments must be numbers');
+    expect(() => randomNumber(1, null)).toThrow('Arguments must be numbers');
+});
